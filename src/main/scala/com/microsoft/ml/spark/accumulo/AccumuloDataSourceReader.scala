111472334cb9458a0f304d6200b6b5952f8c604a
scgraham@microsoft.com <gramhagen@users.noreply.github.com>
2019-08-17 03:22:38 +0000

    updates to accumulo reader

--- src/main/scala/com/microsoft/ml/spark/accumulo/AccumuloDataSourceReader.scala
+++ src/main/scala/com/microsoft/ml/spark/accumulo/AccumuloDataSourceReader.scala
@@ -3,33 +3,44 @@
 
 package com.microsoft.ml.spark.accumulo
 
+import org.apache.accumulo.core.clientImpl.{ClientContext, Tables}
+import org.apache.accumulo.core.dataImpl.KeyExtent
+import org.apache.accumulo.core.metadata.MetadataServicer
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.sources.v2.DataSourceOptions
-import org.apache.spark.sql.sources.v2.reader.DataSourceReader
-import org.apache.spark.sql.sources.v2.reader.InputPartition
-import org.apache.spark.sql.sources.v2.reader.InputPartitionReader
+import org.apache.spark.sql.sources.v2.reader.{DataSourceReader, InputPartition, InputPartitionReader}
 import org.apache.spark.sql.types.StructType
-import java.util.{Arrays, List}
 
 @SerialVersionUID(1L)
-class AccumuloDataSourceReader(val schema: StructType, options: DataSourceOptions) extends DataSourceReader with Serializable {
+class AccumuloDataSourceReader(val schema: StructType, options: DataSourceOptions)
+  extends DataSourceReader with Serializable {
 
-  val props = options.asMap()
-  val tableName = options.tableName.get
+  def readSchema: StructType = schema
 
-  override def readSchema: StructType = schema
+  def planInputPartitions: java.util.List[InputPartition[InternalRow]] = {
+    val tableName = options.tableName.get
+    val props = options.asMap()
+    val properties = new java.util.Properties()
+    properties.putAll(props)
 
-  override def planInputPartitions: List[InputPartition[InternalRow]] = {
-    // TODO: query accumulo server for partitions
-    // https://github.com/apache/accumulo/blob/master/core/src/main/
-    // java/org/apache/accumulo/core/client/mapred/AbstractInputFormat.java#L723
+    // get map of accumulo tabletservers for given table
+    val client = new ClientContext(properties)
+    val tableId = Tables.getTableId(client, tableName)
+    val tabletLocations = new java.util.TreeMap[KeyExtent, String]()
+    // based on org/apache/accumulo/test/performance/scan/CollectTabletStats.java
+    MetadataServicer.forTableId(client, tableId).getTabletLocations(tabletLocations)
 
-    Arrays.asList(new InputPartition[InternalRow]() {
-      override def createPartitionReader: InputPartitionReader[InternalRow] = {
-        // I assume this runs on the exectuors
-        // new Nothing(tableName, props, schema)
-        null
-      }
-    })
+    val partitionReaderFactories = new java.util.ArrayList[InputPartition[InternalRow]]
+    for (_ <- 0 until tabletLocations.size()) {
+      partitionReaderFactories.add(new PartitionReaderFactory(tableName, properties, schema))
+    }
+    partitionReaderFactories
+  }
+}
+
+class PartitionReaderFactory(tableName: String, properties: java.util.Properties, schema: StructType)
+  extends InputPartition[InternalRow] {
+  def createPartitionReader: InputPartitionReader[InternalRow] = {
+    new AccumuloInputPartitionReader(tableName, properties, schema)
   }
 }
