22c051c5dac74aa14ed5dfcb3e3c47c29f02903b
Markus Cozowicz <marcozo@microsoft.com>
2019-08-21 18:04:50 +0200

    switched to nested schema strategy to communicate column family/qualifier

--- src/main/scala/com/microsoft/ml/spark/accumulo/AccumuloInputPartitionReader.scala
+++ src/main/scala/com/microsoft/ml/spark/accumulo/AccumuloInputPartitionReader.scala
@@ -6,66 +6,164 @@ package com.microsoft.ml.spark.accumulo
 import org.apache.accumulo.core.client.IteratorSetting
 import org.apache.accumulo.core.clientImpl.{ClientContext, ScannerImpl, Tables}
 import org.apache.accumulo.core.security.Authorizations
-import org.apache.avro.Schema
+import org.apache.avro.{Schema, SchemaBuilder}
 import org.apache.avro.generic.{GenericData, GenericRecord}
 import org.apache.avro.io.DecoderFactory
 import org.apache.avro.specific.SpecificDatumReader
 import org.apache.spark.sql.avro.AvroDeserializer
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.sources.v2.reader.InputPartitionReader
-import org.apache.spark.sql.types.{DataType, DataTypes, StructType}
+import org.apache.spark.sql.types.{DataType, DataTypes, StringType, StructField, StructType}
 import org.codehaus.jackson.JsonNode
-import com.fasterxml.jackson.databind.ObjectMapper
+import org.codehaus.jackson.map.ObjectMapper
+import org.codehaus.jackson.node.NullNode
 
+import scala.beans.BeanProperty
 import scala.collection.JavaConversions
 
-case class SchemaMappingField(columnFamily: String, columnQualifier: String, typeName: String)
-
-// TODO: remove rowKeyTargetColumn
-case class SchemaMapping(rowKeyTargetColumn: String, mapping: Map[String, SchemaMappingField])
+// keeping the property names short to not hit any limits
+case class SchemaMappingField(
+     @BeanProperty val cf: String,
+     @BeanProperty val cq: String,
+     @BeanProperty val t: String)
 
 @SerialVersionUID(1L)
 object AccumuloInputPartitionReader {
   def catalystSchemaToJson(schema: StructType): String = {
 
-    val mappingFields = schema.fields.map(field => field.name -> SchemaMappingField(
-      field.metadata.getString("cf"),
-      field.metadata.getString("cq"),
-      // TODO: toUpperCase() is weird...
-      catalystToAvroType(field.dataType).getName.toUpperCase
-    )).toMap
+    val selectedFields = schema.fields.flatMap(cf =>
+        cf.dataType match {
+          case cft: StructType => cft.fields.map(cq => SchemaMappingField(
+              cf.name,
+              cq.name,
+              // TODO: toUpperCase() is weird...
+              cq.dataType.typeName.toUpperCase
+            )
+          )
+            // Skip unknown types (e.g. string for row key)
+          case other => None
+          // case other => throw new IllegalArgumentException(s"Unsupported type: ${other}")
+        }
+    )
+      .filter(p => p != None)
 
     try
-      new ObjectMapper().writeValueAsString(SchemaMapping("", mappingFields))
+      new ObjectMapper().writeValueAsString(selectedFields)
     catch {
       case e: Exception =>
         throw new IllegalArgumentException(e)
     }
   }
 
-  def catalystSchemaToAvroSchema(schema: StructType): Schema = {
-    // compile-time method binding. yes it's deprecated. yes it's the only version
-    // available in the spark version deployed
-    val avroFields = schema.fields.map(field =>
-      new Schema.Field(
-        field.name,
-        Schema.create(catalystToAvroType(field.dataType)),
-        null.asInstanceOf[String],
-        null.asInstanceOf[JsonNode]))
+  implicit class CatalystSchemaToAvroRecordBuilder(builder: SchemaBuilder.FieldAssembler[Schema]) {
+    def addAvroRecordFields(schema: StructType): SchemaBuilder.FieldAssembler[Schema] = {
+      schema.fields.foldLeft(builder) { (builder, field) =>
+        field.dataType match {
+          case DataTypes.StringType =>
+            if (field.nullable)
+              builder.optionalString(field.name)
+            else
+              builder.requiredString(field.name)
 
-    Schema.createRecord(JavaConversions.seqAsJavaList(avroFields))
+          case DataTypes.IntegerType =>
+            if (field.nullable)
+              builder.optionalInt(field.name)
+            else
+              builder.requiredInt(field.name)
+
+          case DataTypes.FloatType =>
+            if (field.nullable)
+              builder.optionalFloat(field.name)
+            else
+              builder.requiredFloat(field.name)
+
+          case DataTypes.DoubleType =>
+            if (field.nullable)
+              builder.optionalDouble(field.name)
+            else
+              builder.requiredDouble(field.name)
+
+          case DataTypes.BooleanType =>
+            if (field.nullable)
+              builder.optionalBoolean(field.name)
+            else
+              builder.requiredBoolean(field.name)
+
+          case DataTypes.LongType =>
+            if (field.nullable)
+              builder.optionalLong(field.name)
+            else
+              builder.requiredLong(field.name)
+
+          case DataTypes.BinaryType =>
+            if (field.nullable)
+              builder.optionalBytes(field.name)
+            else
+              builder.requiredBytes(field.name)
+
+          // TODO: date/time support?
+          case other => throw new UnsupportedOperationException(s"Unsupported type: $field.dataType")
+        }
+      }
+    }
   }
 
-  def catalystToAvroType(dataType: DataType): Schema.Type =
-    dataType match {
-      case DataTypes.StringType => Schema.Type.STRING
-      case DataTypes.IntegerType => Schema.Type.INT
-      case DataTypes.FloatType => Schema.Type.FLOAT
-      case DataTypes.DoubleType => Schema.Type.DOUBLE
-      case DataTypes.BooleanType => Schema.Type.BOOLEAN
-      case DataTypes.LongType => Schema.Type.LONG
-      case _ => throw new UnsupportedOperationException(s"Unsupported type: $dataType")
+  def catalystSchemaToAvroSchema(schema: StructType): Schema = {
+    val fieldBuilder = SchemaBuilder.record("root")
+        .fields()
+
+    schema.fields.foldLeft(fieldBuilder) {  (builder, field) =>
+      field.dataType match {
+        case cft: StructType =>
+          // builder.record(field.name).fields().nullableBoolean().endRecord()
+          fieldBuilder
+              .name(field.name)
+            .`type`(SchemaBuilder
+                .record(field.name)
+                .fields
+                  .addAvroRecordFields(cft)
+                .endRecord())
+            .noDefault()
+
+        case other => builder // just skip top-level non-struct fields
+      }
     }
+      .endRecord()
+  }
+//
+//  def catalystSchemaToAvroSchema(schema: StructType, onlyStructs: Boolean = false): java.util.List[Schema.Field] = {
+//    // compile-time method binding for Schema.create. yes it's deprecated. yes it's the only version
+//    // available in the spark version deployed
+//
+//    val avroFields = schema.fields.map(field => {
+//      val avroField = field.dataType match {
+//        case cft: StructType =>
+//          Schema.createRecord(catalystSchemaToAvroSchema(cft))
+//        case other =>
+//          Schema.create(field.dataType match {
+//            case DataTypes.StringType => Schema.Type.STRING
+//            case DataTypes.IntegerType => Schema.Type.INT
+//            case DataTypes.FloatType => Schema.Type.FLOAT
+//            case DataTypes.DoubleType => Schema.Type.DOUBLE
+//            case DataTypes.BooleanType => Schema.Type.BOOLEAN
+//            case DataTypes.LongType => Schema.Type.LONG
+//            case other => throw new UnsupportedOperationException(s"Unsupported type: $field.dataType")
+//          })
+//      }
+//
+//      new Schema.Field(
+//        field.name,
+//        avroField,
+//        null.asInstanceOf[String],
+//        // all fields are nullable for now...
+//        null,
+//        Schema.Field.Order.IGNORE
+//      )
+//    })
+//        .filter(f => !onlyStructs || f.schema().getType().equals(Schema.Type.RECORD))
+//
+//    JavaConversions.seqAsJavaList[Schema.Field](avroFields)
+//  }
 }
 
 @SerialVersionUID(1L)
@@ -97,6 +195,7 @@ class AccumuloInputPartitionReader(val tableName: String,
   // scanner.setRange(baseSplit.getRange());
   private val scannerIterator = scanner.iterator()
 
+  // private val avroSchema = Schema.createRecord(AccumuloInputPartitionReader.catalystSchemaToAvroSchema(schema, true))
   private val avroSchema = AccumuloInputPartitionReader.catalystSchemaToAvroSchema(schema)
   private val deserializer = new AvroDeserializer(avroSchema, schema)
   private val reader = new SpecificDatumReader[GenericRecord](avroSchema)
---
+++ src/test/scala/com/microsoft/ml/spark/accumulo/VerifyAccumuloSchema.scala
@@ -0,0 +1,86 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in project root for information.
+
+package com.microsoft.ml.spark.accumulo
+
+import java.io.ByteArrayOutputStream
+
+import com.microsoft.ml.spark.core.test.base.TestBase
+import org.apache.avro.Schema
+import org.apache.avro.generic.{GenericData, GenericRecord, GenericRecordBuilder}
+import org.apache.avro.io.EncoderFactory
+import org.apache.avro.specific.SpecificDatumWriter
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
+
+
+class VerifyAccumuloSchema extends TestBase {
+  test("Validate catalyst schema to json serialization") {
+    val schema = (new StructType)
+       .add(StructField("cf1", (new StructType)
+          .add("cq1", DataTypes.StringType, true)
+          .add("cq2", DataTypes.DoubleType, true)
+          , true))
+      .add(StructField("cf2", (new StructType)
+        .add("cq_a", DataTypes.IntegerType, true)
+        .add("cq_b", DataTypes.FloatType, true)
+        , true))
+      .add("rowKey", DataTypes.StringType, false)
+
+    val jsonActual = AccumuloInputPartitionReader.catalystSchemaToJson(schema)
+    val jsonExpected = "[{\"cf\":\"cf1\",\"cq\":\"cq1\",\"t\":\"STRING\"}" +
+      ",{\"cf\":\"cf1\",\"cq\":\"cq2\",\"t\":\"DOUBLE\"}" +
+      ",{\"cf\":\"cf2\",\"cq\":\"cq_a\",\"t\":\"INTEGER\"}" +
+      ",{\"cf\":\"cf2\",\"cq\":\"cq_b\",\"t\":\"FLOAT\"}]"
+
+    println(jsonActual)
+    assert(jsonActual.equals(jsonExpected))
+  }
+
+  test("Validate catalyst schema to avro serialization") {
+    val schema = (new StructType)
+      .add(StructField("cf1", (new StructType)
+        .add("cq1", DataTypes.StringType, true)
+        .add("cq2", DataTypes.DoubleType, false)
+        .add("cq3", DataTypes.DoubleType, true)
+        , true))
+      .add(StructField("cf2", (new StructType)
+        .add("cq_a", DataTypes.IntegerType, true)
+        .add("cq_b", DataTypes.FloatType, true)
+        , true))
+      .add("rowKey", DataTypes.StringType, false)
+
+    val avroSchema = AccumuloInputPartitionReader.catalystSchemaToAvroSchema(schema)
+
+    val builder = new GenericRecordBuilder(avroSchema)
+
+    val builderCf1 = new GenericRecordBuilder(avroSchema.getField("cf1").schema())
+    val builderCf2 = new GenericRecordBuilder(avroSchema.getField("cf2").schema())
+    // check if clear() helps perf?
+
+    builderCf1.set("cq1", "foo")
+    builderCf1.set("cq2", 2.3)
+
+    builderCf2.set("cq_a", 1)
+    builderCf2.set("cq_b", 1.2f)
+
+    builder.set("cf1", builderCf1.build())
+    builder.set("cf2", builderCf2.build())
+
+    val output = new ByteArrayOutputStream()
+    val encoder = EncoderFactory.get.jsonEncoder(avroSchema, output)
+
+    val writer = new SpecificDatumWriter[GenericRecord](avroSchema)
+    writer.write(builder.build(), encoder)
+
+    encoder.flush()
+
+    val jsonOutput = new String(output.toByteArray)
+    println(jsonOutput)
+
+    val jsonExpected = "{\"cf1\":{\"cq1\":{\"string\":\"foo\"}," +
+      "\"cq2\":2.3,\"cq3\":null}," +
+      "\"cf2\":{\"cq_a\":{\"int\":1},\"cq_b\":{\"float\":1.2}}}"
+    assert(jsonOutput.equals(jsonExpected))
+  }
+}
+
